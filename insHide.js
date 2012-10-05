bebe571cabf1567016e0c233d6c763d18d65103b
gueno <guenole.marquier@gmail.com>
2012-10-05 19:26:33 +0200

    v1.0

---
+++ insHide.js
@@ -0,0 +1,62 @@
+/*!
+ * insHide jQuery plugin
+ * Original author: @guenoz
+ */
+
+;(function ( $, window, document, undefined ) {
+
+    var pluginName = 'inshide',
+        defaults   = {
+            minimum  : 100,
+            classhin : 'in',
+            classhout: 'out'
+        };
+
+    $.fn.extend({
+         
+        inshide: function(options) {
+                 
+            var options =  $.extend(defaults, options);
+ 
+            return this.each(function() {
+                   
+                var mini = ( options.minimum / 100 ) || 1,
+                       that = $(this),
+                          p = that.parent(),
+                     height = p.outerHeight(),
+                      width = p.outerWidth(),
+                          o = p.offset(),
+                       left = o.left,
+                        top = o.top,
+                        ret = [];
+                        
+                that.children().each(function() {
+                    var c = $(this);
+                    
+                    if(this == document.documentElement)
+                        return ret.push(this);
+                    
+                    var offset = c.offset(),
+                        ew = c.outerWidth(),
+                        eh = c.outerHeight();
+                        
+                    res = !(
+                           ( offset.top  + ( eh * mini ) > top + height )
+                        || ( offset.top  +   eh - ( eh * mini )   < top )
+                        || ( offset.left + ( ew * mini ) > left + width )
+                        || ( offset.left +   ew - ( ew * mini )  < left )
+                    );
+
+                    if(res)
+                        c.removeClass(options.classhout).addClass(options.classhin);
+                    else
+                        c.removeClass(options.classhin).addClass(options.classhout);
+                });
+             
+            });
+            
+        }
+        
+    });
+    
+})( jQuery, window, document );
